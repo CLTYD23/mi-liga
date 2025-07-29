949a7f3532c558dedd97df21e8203ae81d3e2e33
CLTYD23 <8150696+CLTYD23@users.noreply.github.com>
2025-07-29 19:40:32 +0200

    Using style files in components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-import HolaMundo from './components/HolaMundo';
+import HolaMundo from './components/hola-mundo/HolaMundo';
 
 
 render(<HolaMundo nombre="Sergio" edad="35" />, document.querySelector('#root'));
