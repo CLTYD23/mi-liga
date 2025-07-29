b7d37c2e93a77de0a38acce0941bc50ab6e3b73c
CLTYD23 <8150696+CLTYD23@users.noreply.github.com>
2025-07-29 19:45:42 +0200

    Using Material Design Components in React.

--- src/components/hola-mundo/HolaMundo.js
+++ src/components/hola-mundo/HolaMundo.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Button from 'material-ui/Button';
+
 import './HolaMundo.css';
+
 
 class HolaMundo extends React.Component {
     render() {
@@ -7,6 +10,9 @@ class HolaMundo extends React.Component {
             <div>
                 <h2 >Hola {this.props.nombre} </h2>
                 <p> {this.props.edad}  </p>
+
+                <Button variant="fab" color="secondary"> + </Button>
+
                 <p className={this.props.tipo} > Vestibulum lorem quam, facilisis a imperdiet nec, interdum venenatis tortor. Etiam condimentum ligula sit amet fringilla consequat. Praesent ultrices, augue non imperdiet pellentesque, purus nunc malesuada augue, aliquam commodo felis libero id arcu. Donec ex lorem, pellentesque ac maximus ut, ultricies vitae mi. Donec egestas odio vitae sapien facilisis feugiat. Suspendisse potenti. Sed volutpat congue lacinia. Donec quis rhoncus felis, id aliquam elit. Cras scelerisque, quam ut rhoncus bibendum, dolor ipsum sollicitudin ipsum, id luctus lorem neque a lacus. Aenean tristique erat et mauris cursus malesuada. </p>
             </div>
         )
