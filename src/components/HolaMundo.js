973b6ad9ce6e5b52a4038e6d27b5062eea66f0e6
CLTYD23 <8150696+CLTYD23@users.noreply.github.com>
2025-07-29 19:28:36 +0200

    Trying to generate HTML code with JSX.

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class HolaMundo extends React.Component {
     render() {
-        return <p>Hola Mundo</p>
+        return (
+            <div>
+                <p>Hola Mundo</p>
+                <p>Hola Mundo</p>
+                <input />
+                <br />
+                <hr />
+            </div>
+        )
     }
 }
 
